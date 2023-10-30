b057c6c9e304714a519eb9b9b6e98ea95b3a6d81
performoon <technikutryn@gmail.com>
2023-10-30 15:08:51 +0900

    add CORS

--- api/main.py
+++ api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # 追加
 
 from api.routers import user as user_routers
 from api.routers import food as food_routers
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追加
+    allow_methods=["*"],      # 追加
+    allow_headers=["*"]       # 追加
+)
 
 app.include_router(user_routers.router)
 app.include_router(food_routers.router)
--- api/schemas/token.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-#Tokenのデータ型
-class Token(BaseModel):
-    access_token: str
-    token_type: str
